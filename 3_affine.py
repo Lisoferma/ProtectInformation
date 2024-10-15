b54de844584f35c1f9a4d2d98df574d82b62b97f
Lisoferma <pilipeiko.sasha@gmail.com>
2024-10-16 01:12:30 +0900

    Delete unused code

--- 3_affine.py
+++ 3_affine.py
@@ -16,24 +16,6 @@ def modinv(a, m):
         return x % m
 
 
-def vigenere(text, key, is_encrypt=True):
-    alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя '
-    result = ''
-
-    for i in range(len(text)):
-        letter_n = alphabet.index(text[i])
-        key_n = alphabet.index(key[i % len(key)])
-
-        if is_encrypt:
-            value = (letter_n + key_n) % len(alphabet)
-        else:
-            value = (letter_n - key_n) % len(alphabet)
-
-        result += alphabet[value]
-
-    return result
-
-
 def affine(text, a, b, is_encrypt=True):
     alphabet = 'аоуынт '
     a_inv = modinv(a, len(alphabet))
